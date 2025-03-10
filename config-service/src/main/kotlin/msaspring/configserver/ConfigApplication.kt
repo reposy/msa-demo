7d4da0d9781fe83e764a157b01f23f3f23907ffd
syhan <hsykys0728@gmail.com>
2025-03-10 11:30:26 +0900

    docker로 실행하기 위한 작업

--- config-service/src/main/kotlin/msaspring/configserver/ConfigApplication.kt
+++ config-service/src/main/kotlin/msaspring/configserver/ConfigApplication.kt
@@ -2,8 +2,10 @@ package msaspring.configserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class ConfigApplication
 
 fun main(args: Array<String>) {

65db13498d0f716bbe87cad872ae8af9a8c85cb7
syhan <hsykys0728@gmail.com>
2025-03-13 15:42:27 +0900

    dashboard <-> api-gateway 간 cors 설정

---
+++ api-gateway/src/main/kotlin/msaspring/apigateway/CorsConfig.kt
@@ -0,0 +1,15 @@
+package msaspring.apigateway
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+class CorsConfig : WebFluxConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+    }
+}
